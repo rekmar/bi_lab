164930a49fe24fcd414c95f5cc80b85640a9bf5e
Maryia Rekhva <Maryia_Rekhva@epam.com>
2018-05-28 14:12:15 +0300

    delete task2

--- Task_02/FizzBuzz.py
+++
@@ -1,9 +0,0 @@
-for i in range(1, 100):
-    out = ""
-    if i % 3 == 0:
-        out += "Fizz"
-    if i % 5 == 0:
-        out += "Buzz"
-    if out == "":
-        out += str(i)
-    print(out, end=" ")
