261814f4978148d8dd5e7cdc25295c4f679e0713
rekmar <37531141+rekmar@users.noreply.github.com>
2018-06-03 13:41:54 +0300

    Update replacing.py

--- Task_07/replacing.py
+++ Task_07/replacing.py
@@ -2,7 +2,7 @@ while True:
     enter = input("\nTo quit enter -1, to continue press any key\n")
     if enter != "-1":
         text = list(input("Enter text: "))
-        for i in range(0, len(text)-1):
+        for i in range(0, len(text) - 1):
             if text[i] == ' ':
                 text[i] = '_'
             elif text[i] == '_':
